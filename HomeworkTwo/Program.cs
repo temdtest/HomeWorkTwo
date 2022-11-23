d1a086c08a65b0192eaad7b99beb511ef15b29e6
Theis Madsen <temd@simcorp.com>
2022-11-23 15:41:43 +0100

    Homework 2. Assignment 2.

--- HomeworkTwo/Program.cs
+++ HomeworkTwo/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HomeworkTwo
 {
     class Program
     {
-        /// TASK: Three parameters: Nominal of the transaction, Trade price, Transaction Type (Buy, sell), Sign (1 for buy, -1 for sell)
+        // TASK: Three parameters: Nominal of the transaction, Trade price, Transaction Type (Buy, sell), Sign (1 for buy, -1 for sell)
         enum Transactiontype
         {
             Buy = 1,
@@ -18,15 +21,31 @@ namespace HomeworkTwo
 
             Console.WriteLine("\nPlease enter the trade price: ");
             string tradeInput = Console.ReadLine();
-            int price = Int32.Parse(tradeInput);
+            int tradePrice = Int32.Parse(tradeInput);
 
             Console.WriteLine("\nWould you like to buy or sell the securities?: ");
             Transactiontype trcType;
             userInput = Console.ReadLine();
             trcType = (Transactiontype) Enum.Parse(typeof(Transactiontype), userInput,true);
 
-            var currencyValue = (int)trcType * nominal * price;
-            Console.WriteLine($"\nThe current value based on your wish to {trcType} is {currencyValue}");
+            switch (trcType)
+            {
+                case Transactiontype.Buy:
+                    var currentValue = (int)trcType * nominal * tradePrice;
+                    Console.WriteLine($"\nThe current value based on your wish to {trcType} is {currentValue}");
+                    break;
+                
+                case Transactiontype.Sell:
+                    Console.WriteLine("\nPlease input the original purchase price of the security: ");
+                    string originalInput = Console.ReadLine();
+                    int originalPrice = Int32.Parse(originalInput);
+                    var profitAndLoss = (tradePrice - originalPrice) * nominal;
+                    Console.WriteLine(profitAndLoss == 0
+                        ? $"\nThe P/L is {profitAndLoss} for the chosen security"
+                        : $"\nBased on your wish to {trcType} the P/L has been calculated to: {profitAndLoss}");
+                    break;
+                
+            }
             Console.ReadKey();
         }
     }
